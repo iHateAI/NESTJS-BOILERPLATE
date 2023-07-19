7fe6669108f7b643d9bcf79eb9f9ecb4090335cc
HyunSeong <bolton7425@gmail.com>
2023-07-20 02:19:23 +0900

    FEAT: TypeOrmModule 등록

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: parseInt(process.env.MYSQL_PORT, 10),
+      username: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: 'test',
+      synchronize: true,
+      logging: true,
+      entities: [],
     }),
   ],
   controllers: [AppController],
